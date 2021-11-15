2bd79af412b6444e915ae8333ce6f4df83b1dc36
Wasi Mohammed Abdullah <wasi0013@gmail.com>
2021-11-15 20:02:12 +0600

    added integer input parser

--- aoc/lib/helper/io.ex
+++ aoc/lib/helper/io.ex
@@ -1,11 +1,7 @@
 defmodule Aoc.Helper.IO do
   @moduledoc false
   @spec get_string_input(String, String) :: any
-  def get_string_input(year, day) do
-     case File.read("priv/resources/#{year}/day#{day}.txt") do
-      {:ok, data} ->  String.trim(data)
-      {:error, reason} -> raise reason
-    end
-
-  end
+  def get_string_input(year, day), do: File.read("priv/resources/#{year}/day#{day}.txt") |> elem(1)|>  String.trim
+  def get_integer_input(year, day), do:
+      get_string_input(year, day) |> String.split(" ") |> Enum.map(&String.to_integer/1)
 end
