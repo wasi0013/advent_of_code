0bbc4ae0e4ad9cfdf311ef14a7456f1f732d572c
Wasi Mohammed Abdullah <wasi0013@gmail.com>
2021-11-15 22:33:09 +0600

    added test for 2018 day 08

---
+++ aoc/test/y2018/day_08_test.exs
@@ -0,0 +1,19 @@
+defmodule AocTest.Y2018.Day08Test do
+  @moduledoc false
+  @example [2, 3, 0, 3, 10, 11, 12, 1, 1, 0, 1, 99, 2, 1, 1, 2]
+
+  use ExUnit.Case
+  test "Part1 Example: #{@example}" do
+    assert Aoc.Y2018.Day08.solve_part1(@example) == 138
+  end
+  test "Part2 Example: #{@example}" do
+    assert Aoc.Y2018.Day08.solve_part2(@example) == 66
+  end
+  test "Part 1 is solved" do
+    assert Aoc.Y2018.Day08.run_part1() == 46_829
+  end
+  test "Part 2 is solved" do
+    assert Aoc.Y2018.Day08.run_part2() == 37_450
+  end
+
+end
