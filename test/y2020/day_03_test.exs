9de5c650a19bf45fd946003edec218094974b7ff
Wasi Mohammed Abdullah <wasi0013@gmail.com>
2021-11-20 05:25:01 +0600

    fixed testcase

--- test/y2020/day_03_test.exs
+++ test/y2020/day_03_test.exs
@@ -7,6 +7,6 @@ defmodule AocTest.Y2020.Day03Test do
   end
 
   test "Part 2 is solved" do
-    assert Aoc.Y2020.Day02.run_part2() == 3_584_591_857
+    assert Aoc.Y2020.Day03.run_part2() == 3_584_591_857
   end
 end
