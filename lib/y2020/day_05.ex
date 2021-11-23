b1daf68027dda98ec456e5737b923be60ea75f1c
Wasi Mohammed Abdullah <wasi0013@gmail.com>
2021-11-23 17:22:17 +0600

    refactor function name

--- lib/y2020/day_05.ex
+++ lib/y2020/day_05.ex
@@ -20,15 +20,13 @@ defmodule Aoc.Y2020.Day05 do
   end
 
   defp get_seat_id(seat),
-    do:
-      binary_search(Enum.slice(seat, 0..6), Enum.to_list(0..127)) * 8 +
-        binary_search(Enum.slice(seat, 7..9), Enum.to_list(0..7))
+    do: search(Enum.slice(seat, 0..6), Enum.to_list(0..127)) * 8 + search(Enum.slice(seat, 7..9), Enum.to_list(0..7))
 
-  defp binary_search([], list), do: hd(list)
-  defp binary_search(["F" | rest], list), do: binary_search(rest, Enum.split(list, round(length(list) / 2)) |> elem(0))
-  defp binary_search(["B" | rest], list), do: binary_search(rest, Enum.split(list, round(length(list) / 2)) |> elem(1))
-  defp binary_search(["L" | rest], list), do: binary_search(rest, Enum.split(list, round(length(list) / 2)) |> elem(0))
-  defp binary_search(["R" | rest], list), do: binary_search(rest, Enum.split(list, round(length(list) / 2)) |> elem(1))
+  defp search([], list), do: hd(list)
+  defp search(["F" | rest], list), do: search(rest, Enum.split(list, round(length(list) / 2)) |> elem(0))
+  defp search(["B" | rest], list), do: search(rest, Enum.split(list, round(length(list) / 2)) |> elem(1))
+  defp search(["L" | rest], list), do: search(rest, Enum.split(list, round(length(list) / 2)) |> elem(0))
+  defp search(["R" | rest], list), do: search(rest, Enum.split(list, round(length(list) / 2)) |> elem(1))
 
   @spec get_input() :: no_return
   defp get_input(), do: get_string_input("2020", "05") |> String.split("\n") |> Enum.map(&String.graphemes/1)
