1dd576f40cf84c1875c1614dc0466a306c639544
Wasi Mohammed Abdullah <wasi0013@gmail.com>
2021-11-23 17:12:39 +0600

    solved day 05 2020

---
+++ test/y2020/day_05_test.exs
@@ -0,0 +1,17 @@
+defmodule AocTest.Y2020.Day05Test do
+  @moduledoc false
+  use ExUnit.Case
+  import Aoc.Y2020.Day05
+  @example [["F", "B", "F", "B", "B", "F", "F", "L", "R", "L"]]
+  test "Part 1 Example" do
+    assert solve_part1(@example) == 354
+  end
+
+  test "Part 1 is solved" do
+    assert run_part1() == 864
+  end
+
+  test "Part 2 is solved" do
+    assert run_part2() == 739
+  end
+end
