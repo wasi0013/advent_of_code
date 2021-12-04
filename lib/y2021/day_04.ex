aa086fa11890415dbadc2578baa3895d74b16f44
Wasi Mohammed Abdullah <wasi0013@gmail.com>
2021-12-04 15:38:25 +0600

    refactored day 04 2021

--- lib/y2021/day_04.ex
+++ lib/y2021/day_04.ex
@@ -14,44 +14,44 @@ defmodule Aoc.Y2021.Day04 do
     solve_part1({rest, marked_boards}, winner)
   end
 
-  def solve_part1({_, _}, {[board | _rest], num}), do: calculate_result({board, num})
+  def solve_part1({_, _}, {[board | _rest], num}), do: calculate_cache({board, num})
 
-  defp calculate_result({board, num}),
+  defp calculate_cache({board, num}),
     do: board |> List.flatten() |> Enum.reject(&(&1 === true)) |> Enum.sum() |> then(fn total -> num * total end)
 
-  def solve_part2({[], _marked_boards}, result, winner), do: calculate_result(Map.get(result, hd(winner)))
+  def solve_part2({[], _marked_boards}, cache, winners), do: calculate_cache(Map.get(cache, hd(winners)))
 
-  def solve_part2({[num | rest], boards}, result, winner) do
+  def solve_part2({[num | rest], boards}, cache, winners) do
     marked_boards = boards |> Enum.map(&mark_board(&1, num))
-    result = update_result(marked_boards, num, marked_boards, result)
-    winner = update_winner(marked_boards, marked_boards, winner)
-    solve_part2({rest, marked_boards}, result, winner)
+    cache = update_cache(marked_boards, num, marked_boards, cache)
+    winners = update_winners(marked_boards, marked_boards, winners)
+    solve_part2({rest, marked_boards}, cache, winners)
   end
 
-  defp update_winner([], _, winner), do: winner
+  defp update_winners([], _, winners), do: winners
 
-  defp update_winner([board | boards], marked_boards, winner) do
-    if Enum.find_index(marked_boards, &(&1 == board)) in winner do
-      update_winner(boards, marked_boards, winner)
+  defp update_winners([board | boards], marked_boards, winners) do
+    if Enum.find_index(marked_boards, &(&1 == board)) in winners do
+      update_winners(boards, marked_boards, winners)
     else
       if check_winner?(board),
-        do: update_winner(boards, marked_boards, [Enum.find_index(marked_boards, &(&1 == board)) | winner]),
-        else: update_winner(boards, marked_boards, winner)
+        do: update_winners(boards, marked_boards, [Enum.find_index(marked_boards, &(&1 == board)) | winners]),
+        else: update_winners(boards, marked_boards, winners)
     end
   end
 
-  defp update_result([], _num, _marked_boards, result), do: result
+  defp update_cache([], _num, _marked_boards, cache), do: cache
 
-  defp update_result([board | boards], num, marked_boards, result),
-    do: update_result(boards, num, marked_boards, get_new_result(num, board, marked_boards, result))
+  defp update_cache([board | boards], num, marked_boards, cache),
+    do: update_cache(boards, num, marked_boards, get_new_cache(num, board, marked_boards, cache))
 
-  defp get_new_result(num, board, marked_boards, result) do
-    if Enum.find_index(marked_boards, &(&1 == board)) in Map.keys(result) do
-      result
+  defp get_new_cache(num, board, marked_boards, cache) do
+    if Enum.find_index(marked_boards, &(&1 == board)) in Map.keys(cache) do
+      cache
     else
       if check_winner?(board),
-        do: Map.put_new(result, Enum.find_index(marked_boards, &(&1 == board)), {board, num}),
-        else: result
+        do: Map.put_new(cache, Enum.find_index(marked_boards, &(&1 == board)), {board, num}),
+        else: cache
     end
   end
 
