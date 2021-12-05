edc8de331c8760f6c2494748831b018d87689280
Wasi Mohammed Abdullah <wasi0013@gmail.com>
2021-12-05 14:29:57 +0600

    refactored day 05 2021

--- lib/y2021/day_05.ex
+++ lib/y2021/day_05.ex
@@ -11,81 +11,45 @@ defmodule Aoc.Y2021.Day05 do
   def solve_part1(data) do
     data
     |> Enum.filter(fn [x1, y1, x2, y2] -> x1 == x2 or y1 == y2 end)
-    |> populate_grid(gen_grid(get_max(data)))
+    |> populate_grid(data |> get_max() |> gen_grid())
     |> Map.values()
     |> Enum.count(fn i -> i > 1 end)
   end
 
   def solve_part2(data) do
     data
-    |> populate_grid(gen_grid(get_max(data)))
+    |> populate_grid(data |> get_max() |> gen_grid())
     |> Map.values()
-    |> Enum.count(fn i -> i > 1 end)
+    |> Enum.count(&(&1 > 1))
   end
 
   defp populate_grid([], grid), do: grid
+  defp populate_grid([[x, x, y, y] | data], grid), do: populate_grid(data, update_grid(x, y, grid))
+  defp populate_grid([[x, y1, x, y2] | data], grid), do: populate_grid(data, update_grid({x, {y1, y2}}, grid))
+  defp populate_grid([[x1, y, x2, y] | data], grid), do: populate_grid(data, update_grid({{x1, x2}, y}, grid))
 
-  defp populate_grid([[x, x, y, y] | data], grid) do
-    populate_grid(data, update_grid(x, y, grid))
-  end
-
-  defp populate_grid([[x, y1, x, y2] | data], grid) do
-    populate_grid(data, update_grid({x, {y1, y2}}, grid))
-  end
-
-  defp populate_grid([[x1, y, x2, y] | data], grid) do
-    populate_grid(data, update_grid({{x1, x2}, y}, grid))
-  end
-
-  defp populate_grid([[x1, y1, x2, y2] | data], grid) when abs(x1 - x2) == abs(y1 - y2) do
-    grid = populate_diagonals(x1, y1, x2, y2, grid)
-    populate_grid(data, grid)
-  end
+  defp populate_grid([[x1, y1, x2, y2] | data], grid) when abs(x1 - x2) == abs(y1 - y2),
+    do: populate_grid(data, populate_diagonals(x1, y1, x2, y2, grid))
 
   defp populate_diagonals(x, y, x, y, grid), do: bump_grid({x, y}, grid)
 
-  defp populate_diagonals(x1, y1, x2, y2, grid) do
-    grid = bump_grid({x1, y1}, grid)
-    x1 = if x1 > x2, do: x1 - 1, else: x1 + 1
-    y1 = if y1 > y2, do: y1 - 1, else: y1 + 1
-    populate_diagonals(x1, y1, x2, y2, grid)
-  end
+  defp populate_diagonals(x1, y1, x2, y2, grid),
+    do: populate_diagonals(shift(x1, x2), shift(y1, y2), x2, y2, bump_grid({x1, y1}, grid))
 
-  defp update_grid(x, y, grid) do
-    if x > y do
-      update_grid(x, y + 1, bump_grid({y, y}, grid))
-    else
-      if x < y do
-        update_grid(x + 1, y, bump_grid({x, x}, grid))
-      else
-        bump_grid({x, x}, grid)
-      end
-    end
-  end
+  defp shift(x1, x2) when x1 > x2, do: x1 - 1
+  defp shift(x1, _x2), do: x1 + 1
 
-  defp update_grid({x, {y1, y2}}, grid) do
-    if y1 > y2 do
-      update_grid({x, {y1, y2 + 1}}, bump_grid({x, y2}, grid))
-    else
-      if y1 < y2 do
-        update_grid({x, {y1 + 1, y2}}, bump_grid({x, y1}, grid))
-      else
-        bump_grid({x, y1}, grid)
-      end
-    end
-  end
+  defp update_grid(x, y, grid) when x > y, do: update_grid(x, y + 1, bump_grid({y, y}, grid))
+  defp update_grid(x, y, grid) when x < y, do: update_grid(x + 1, y, bump_grid({x, x}, grid))
+  defp update_grid(x, x, grid), do: bump_grid({x, x}, grid)
 
-  defp update_grid({{x1, x2}, y}, grid) do
-    if x1 > x2 do
-      update_grid({{x1, x2 + 1}, y}, bump_grid({x2, y}, grid))
-    else
-      if x1 < x2 do
-        update_grid({{x1 + 1, x2}, y}, bump_grid({x1, y}, grid))
-      else
-        bump_grid({x1, y}, grid)
-      end
-    end
-  end
+  defp update_grid({x, {y1, y2}}, grid) when y1 > y2, do: update_grid({x, {y1, y2 + 1}}, bump_grid({x, y2}, grid))
+  defp update_grid({x, {y1, y2}}, grid) when y1 < y2, do: update_grid({x, {y1 + 1, y2}}, bump_grid({x, y1}, grid))
+  defp update_grid({x, {y, y}}, grid), do: bump_grid({x, y}, grid)
+
+  defp update_grid({{x1, x2}, y}, grid) when x1 > x2, do: update_grid({{x1, x2 + 1}, y}, bump_grid({x2, y}, grid))
+  defp update_grid({{x1, x2}, y}, grid) when x1 < x2, do: update_grid({{x1 + 1, x2}, y}, bump_grid({x1, y}, grid))
+  defp update_grid({{x, x}, y}, grid), do: bump_grid({x, y}, grid)
 
   defp bump_grid({x, y}, grid), do: Map.update(grid, {x, y}, 0, &(&1 + 1))
 
