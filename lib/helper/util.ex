5356781d7fa7897ef83a8295a2bc8b752494eb05
Wasi Mohammed Abdullah <wasi0013@gmail.com>
2021-12-09 01:41:13 +0600

    fixed formatting

--- lib/helper/util.ex
+++ lib/helper/util.ex
@@ -4,5 +4,4 @@ defmodule Aoc.Helper.Util do
   def transpose(matrix), do: matrix |> Enum.zip() |> Enum.map(&Tuple.to_list/1)
   def permutations([]), do: [[]]
   def permutations(elements), do: for(elem <- elements, rest <- permutations(elements -- [elem]), do: [elem | rest])
-
 end
--- lib/y2021/day_08.ex
+++ lib/y2021/day_08.ex
@@ -5,18 +5,7 @@ defmodule Aoc.Y2021.Day08 do
   import Aoc.Helper.IO
   import Aoc.Helper.Util
 
-  @digits %{
-    abcdefg: 8,
-    acf: 7,
-    abdefg: 6,
-    abdfg: 5,
-    bcdf: 4,
-    acdfg: 3,
-    acdeg: 2,
-    cf: 1,
-    abcefg: 0,
-    abcdfg: 9
-  }
+  @digits %{abcdefg: 8, acf: 7, abdefg: 6, abdfg: 5, bcdf: 4, acdfg: 3, acdeg: 2, cf: 1, abcefg: 0, abcdfg: 9}
   @default_segments ["a", "b", "c", "d", "e", "f", "g"]
 
   def run_part1(), do: get_input() |> solve_part1()
@@ -38,6 +27,7 @@ defmodule Aoc.Y2021.Day08 do
 
   defp fix_output([digits, outputs], perms), do: find_value(perms, digits, outputs, nil)
   defp find_value(_perms, _digits, _outputs, output) when output != nil, do: output
+
   defp find_value([perm | rest], digits, outputs, _output) do
     output =
       if MapSet.subset?(MapSet.new(to_atoms(digits, perm)), MapSet.new(Map.keys(@digits))) do
@@ -47,20 +37,26 @@ defmodule Aoc.Y2021.Day08 do
         |> Enum.map(fn {digit, p} -> p * Map.get(@digits, digit, 0) end)
         |> Enum.sum()
       end
+
     find_value(rest, digits, outputs, output)
   end
-  defp to_atoms(digits, perm), do:
-    digits
-    |> String.split(" ", trim: true)
-    |> Enum.map(&String.graphemes/1)
-    |> Enum.map(&(to_atom(&1, perm)))
+
+  defp to_atoms(digits, perm),
+    do:
+      digits
+      |> String.split(" ", trim: true)
+      |> Enum.map(&String.graphemes/1)
+      |> Enum.map(&to_atom(&1, perm))
+
   defp to_atom(digit, perm) do
     Enum.map(digit, fn v -> Map.get(Map.new(Enum.zip(@default_segments, perm)), v) end)
     |> Enum.sort()
     |> Enum.join("")
     |> String.to_atom()
   end
+
   defp get_input(),
     do: get_string_input("2021", "08") |> String.split("\n") |> Enum.map(&String.split(&1, "|", trim: true))
+
   def solved_status(), do: :solved
 end
