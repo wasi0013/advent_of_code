54040e1685d339e529398fc3622d818c36695411
Wasi Mohammed Abdullah <wasi0013@gmail.com>
2021-12-11 15:40:09 +0600

    improved formatting

--- lib/y2021/day_11.ex
+++ lib/y2021/day_11.ex
@@ -7,41 +7,34 @@ defmodule Aoc.Y2021.Day11 do
   def run_part1(), do: get_input() |> solve_part1()
   def run_part2(), do: get_input() |> solve_part2()
 
-  def solve_part1(data) do
-    grid(data)
-    |> simulate(100, 0)
-  end
+  def solve_part1(data), do: grid(data) |> simulate(100, 0)
+  def solve_part2(data), do: grid(data) |> find_synchronized_flash()
 
-  def solve_part2(data) do
-    grid(data)
-    |> find_synchronized_flash()
-  end
+  defp simulate(_, 0, count), do: count
 
-  def simulate(_, 0, count), do: count
-
-  def simulate(grid, steps, count) do
+  defp simulate(grid, steps, count) do
     grid = grid |> increase_one() |> increase_adjacent([])
     simulate(set_zero(grid), steps - 1, count + count_flash(grid))
   end
 
-  def find_synchronized_flash(grid, steps \\ 1) do
+  defp find_synchronized_flash(grid, steps \\ 1) do
     grid = grid |> increase_one() |> increase_adjacent([])
     if count_flash(grid) == 100, do: steps, else: find_synchronized_flash(set_zero(grid), steps + 1)
   end
 
-  def grid(data) do
+  defp grid(data) do
     Map.new(
       for i <- 0..(length(data) - 1), j <- 0..(length(Enum.at(data, 0)) - 1), do: {{i, j}, Enum.at(Enum.at(data, i), j)}
     )
   end
 
-  def increase_one(grid) do
+  defp increase_one(grid) do
     for i <- 0..9, j <- 0..9, reduce: grid do
       grid -> Map.update(grid, {i, j}, nil, fn value -> value + 1 end)
     end
   end
 
-  def increase_adjacent(grid, visited) do
+  defp increase_adjacent(grid, visited) do
     {new_grid, visited} =
       Enum.reduce(grid, {grid, visited}, fn {{i, j}, v}, {grid, visited} ->
         if v > 9 and {i, j} not in visited do
@@ -58,7 +51,7 @@ defmodule Aoc.Y2021.Day11 do
     end
   end
 
-  def update_adjacent(grid, {i, j}) do
+  defp update_adjacent(grid, {i, j}) do
     for [x, y] <- [[0, 1], [0, -1], [1, 0], [-1, 0], [-1, -1], [-1, 1], [1, -1], [1, 1]], reduce: grid do
       grid ->
         if i + x <= 9 and i + x >= 0 and j + y <= 9 and j + y >= 0 do
@@ -69,11 +62,11 @@ defmodule Aoc.Y2021.Day11 do
     end
   end
 
-  def count_flash(grid) do
+  defp count_flash(grid) do
     Enum.count(Map.values(grid), fn v -> v > 9 end)
   end
 
-  def set_zero(grid) do
+  defp set_zero(grid) do
     Enum.reduce(grid, grid, fn {{i, j}, _v}, grid ->
       Map.update(grid, {i, j}, nil, fn value -> if value > 9, do: 0, else: value end)
     end)
