619ab3993bc2a222c7ea88679b6dd5006ffbaf85
Wasi Mohammed Abdullah <wasi0013@gmail.com>
2021-12-11 20:15:08 +0600

    reimplement day 08 2021

--- lib/y2021/day_08.ex
+++ lib/y2021/day_08.ex
@@ -3,10 +3,9 @@ defmodule Aoc.Y2021.Day08 do
   Solved https://adventofcode.com/2021/day/8
   """
   import Aoc.Helper.IO
-  import Aoc.Helper.Util
 
   @digits %{abcdefg: 8, acf: 7, abdefg: 6, abdfg: 5, bcdf: 4, acdfg: 3, acdeg: 2, cf: 1, abcefg: 0, abcdfg: 9}
-  @default_segments [?a, ?b, ?c, ?d, ?e, ?f, ?g]
+  @default_segment "abcdefg acf abdefg abdfg bcdf acdfg acdeg cf abcefg abcdfg"
 
   def run_part1(), do: get_input() |> solve_part1()
   def run_part2(), do: get_input() |> solve_part2()
@@ -19,43 +18,26 @@ defmodule Aoc.Y2021.Day08 do
     |> Enum.count()
   end
 
-  def solve_part2(data) do
-    data
-    |> Enum.map(&fix_output(&1, permutations(@default_segments)))
+  def solve_part2(data), do: data |> Enum.map(&fix_output(&1, get_map())) |> Enum.sum()
+
+  defp get_map(),
+    do:
+      @default_segment
+      |> String.split(" ")
+      |> Enum.map(&{&1, get_fingerprint(get_frequencies(@default_segment), &1)})
+      |> Map.new(fn {k, v} -> {v, Map.get(@digits, String.to_atom(k))} end)
+
+  defp fix_output([digits, outputs], digit_map) do
+    outputs
+    |> String.split(" ", trim: true)
+    |> Enum.zip([1000, 100, 10, 1])
+    |> Enum.map(&to_number(&1, digit_map, digits))
     |> Enum.sum()
   end
 
-  defp fix_output([digits, outputs], perms), do: find_value(perms, digits, outputs, nil)
-
-  defp find_value(_perms, _digits, _outputs, output) when output != nil do
-    output
-  end
-
-  defp find_value([perm | rest], digits, outputs, _output) do
-    output =
-      if MapSet.subset?(MapSet.new(to_atoms(digits, perm)), MapSet.new(Map.keys(@digits))) do
-        outputs
-        |> to_atoms(perm)
-        |> Enum.zip([1000, 100, 10, 1])
-        |> Enum.map(fn {digit, p} -> p * Map.get(@digits, digit, 0) end)
-        |> Enum.sum()
-      end
-
-    find_value(rest, digits, outputs, output)
-  end
-
-  defp to_atoms(digits, perm),
-    do:
-      digits
-      |> String.split(" ", trim: true)
-      |> Enum.map(&String.to_charlist/1)
-      |> Enum.map(&to_atom(&1, perm))
-
-  defp to_atom(digit, perm) do
-    Enum.map(digit, fn v -> Map.get(Map.new(Enum.zip(@default_segments, perm)), v) end)
-    |> Enum.sort()
-    |> List.to_atom()
-  end
+  defp to_number({k, c}, digit_map, digits), do: Map.get(digit_map, get_fingerprint(get_frequencies(digits), k)) * c
+  defp get_frequencies(digits), do: Enum.frequencies(String.graphemes(String.replace(digits, " ", "")))
+  defp get_fingerprint(map, digit), do: digit |> String.graphemes() |> Enum.map(&Map.get(map, &1, 0)) |> Enum.sum()
 
   defp get_input(),
     do: get_string_input("2021", "08") |> String.split("\n") |> Enum.map(&String.split(&1, "|", trim: true))
