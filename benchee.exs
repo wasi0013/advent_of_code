a077ca4d5cf6213c9f4adcbaab497407482c7b84
Wasi Mohammed Abdullah <wasi0013@gmail.com>
2021-12-11 23:15:42 +0600

    added benchee to benchmark and compare different days and years

---
+++ benchee.exs
@@ -0,0 +1,16 @@
+part1 = :application.get_key(:aoc, :modules)
+    |> elem(1)
+    |> Enum.filter(&(&1 |> Module.split() |> Enum.at(-1) |> String.starts_with?("Day")))
+    |> Enum.map(fn module -> {module |> Module.split() |> Enum.join(" "), fn -> module.run_part1() end} end)
+    |> Map.new()
+IO.puts("Benchmarking part 1")
+Benchee.run(part1)
+part2 = :application.get_key(:aoc, :modules)
+    |> elem(1)
+    |> Enum.filter(&(&1 |> Module.split() |> Enum.at(-1) |> String.starts_with?("Day")))
+    |> Enum.map(fn module -> {module |> Module.split() |> Enum.join(" "), fn -> module.run_part2() end} end)
+    |> Map.new()
+
+
+IO.puts("Benchmarking part 2")
+Benchee.run(part2)
