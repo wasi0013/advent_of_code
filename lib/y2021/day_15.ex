136ab27b4fbe372e16c8652f3e8d7f0db5d484c1
Wasi Mohammed Abdullah <wasi0013@gmail.com>
2021-12-16 02:37:30 +0600

    replace agent with map

--- lib/y2021/day_15.ex
+++ lib/y2021/day_15.ex
@@ -3,33 +3,24 @@ defmodule Aoc.Y2021.Day15 do
   Solved https://adventofcode.com/2021/day/15
   """
   import Aoc.Helper.IO
-  use Agent
-
-  def start, do: Agent.start_link(fn -> %{} end, name: __MODULE__)
 
   def run_part1(), do: get_input() |> solve_part1()
   def run_part2(), do: get_input() |> solve_part2()
 
   def solve_part1(data) do
-    {:ok, _} = Aoc.Y2021.Day15.start()
-    result = find_path([{0, 0, 0}], data, length(data), length(Enum.at(data, 0)), 1) - Enum.at(Enum.at(data, 0), 0)
-    Agent.stop(__MODULE__, :normal)
-    result
+    find_path([{0, 0, 0}], data, %{}, length(data), length(Enum.at(data, 0)), 1) - Enum.at(Enum.at(data, 0), 0)
   end
 
   def solve_part2(data) do
-    {:ok, _} = Aoc.Y2021.Day15.start()
-    result = find_path([{0, 0, 0}], data, length(data), length(Enum.at(data, 0)), 5) - Enum.at(Enum.at(data, 0), 0)
-    Agent.stop(__MODULE__, :normal)
-    result
+    find_path([{0, 0, 0}], data, %{}, length(data), length(Enum.at(data, 0)), 5) - Enum.at(Enum.at(data, 0), 0)
   end
 
-  def find_path([{_risk_level, row, column} | pqueue], data, row_size, column_size, n)
+  def find_path([{_risk_level, row, column} | pqueue], data, cache, row_size, column_size, n)
       when row < 0 or row >= row_size * n or column < 0 or column >= column_size * n do
-    find_path(pqueue, data, row_size, column_size, n)
+    find_path(pqueue, data, cache, row_size, column_size, n)
   end
 
-  def find_path([{risk_level, row, column} | pqueue], data, row_size, column_size, n) do
+  def find_path([{risk_level, row, column} | pqueue], data, cache, row_size, column_size, n) do
     total_risk =
       Enum.at(Enum.at(data, rem(row, row_size)), rem(column, column_size)) + div(row, row_size) +
         div(column, column_size)
@@ -39,22 +30,22 @@ defmodule Aoc.Y2021.Day15 do
         do: risk_level + rem(total_risk, 9),
         else: risk_level + total_risk
 
-    if Agent.get(__MODULE__, &Map.get(&1, {row, column}, nil)) > total_risk do
-      Agent.update(__MODULE__, &Map.update(&1, {row, column}, total_risk, fn _ -> total_risk end))
+    if Map.get(cache, {row, column}) > total_risk do
+      cache = Map.update(cache, {row, column}, total_risk, fn _ -> total_risk end)
 
       if row == row_size * n - 1 and column == column_size * n - 1 do
-        Agent.get(__MODULE__, &Map.get(&1, {row, column}))
+        Map.get(cache, {row, column})
       else
         pqueue =
           pqueue ++
             Enum.map([[-1, 0], [0, 1], [1, 0], [0, -1]], fn [x, y] ->
-              {Agent.get(__MODULE__, &Map.get(&1, {row, column})), row + x, column + y}
+              {Map.get(cache, {row, column}), row + x, column + y}
             end)
 
-        find_path(Enum.sort(pqueue), data, row_size, column_size, n)
+        find_path(Enum.sort(pqueue), data, cache, row_size, column_size, n)
       end
     else
-      find_path(pqueue, data, row_size, column_size, n)
+      find_path(pqueue, data, cache, row_size, column_size, n)
     end
   end
 
