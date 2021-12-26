5f28dea4bbc8665429e0940eba4d9c34d4bf61f3
Wasi Mohammed Abdullah <wasi0013@gmail.com>
2021-12-26 09:01:15 +0600

    solved day 19, 2021

--- lib/y2021/day_19.ex
+++ lib/y2021/day_19.ex
@@ -1,23 +1,206 @@
-defmodule Aoc.Y2021.Day19  do
+defmodule Aoc.Y2021.Day19 do
   @moduledoc """
   Solved https://adventofcode.com/2021/day/19
   """
   import Aoc.Helper.IO
-  
+
   def run_part1(), do: get_input() |> solve_part1()
   def run_part2(), do: get_input() |> solve_part2()
 
-  def solve_part1(_data) do
-    :unsolved
+  def solve_part1(scanners), do: scanners |> count()
+  def solve_part2(scanners), do: scanners |> find_largest_distance()
+
+  def count({[], beacons}), do: beacons |> MapSet.size()
+
+  def count({scanners, beacons}) do
+    Enum.reduce(scanners, {scanners, beacons}, fn scanner, {rest, beacons} ->
+      if (matched = find_match_point(scanner, beacons)) != [] do
+        {List.delete(rest, scanner), update_beacons(matched, beacons, scanner)}
+      else
+        {rest, beacons}
+      end
+    end)
+    |> count()
   end
-  
-  def solve_part2(_data) do
-    :unsolved
+
+  def count(scanners), do: count({scanners, scanners |> hd |> MapSet.new()})
+
+  def find_largest_distance({[], _beacons, points}),
+    do:
+      Enum.flat_map(points, fn point1 -> Enum.map(points, fn point2 -> manhatten_distance(point1, point2) end) end)
+      |> Enum.max()
+
+  def find_largest_distance({scanners, beacons, points}) do
+    Enum.reduce(scanners, {scanners, beacons, points}, fn scanner, {rest, beacons, points} ->
+      if (matched = find_match_point(scanner, beacons)) != [] do
+        {List.delete(rest, scanner), update_beacons(matched, beacons, scanner), [matched |> hd() |> elem(0) | points]}
+      else
+        {rest, beacons, points}
+      end
+    end)
+    |> find_largest_distance()
+  end
+
+  def find_largest_distance(scanners), do: find_largest_distance({scanners, scanners |> hd |> MapSet.new(), []})
+
+  def update_beacons([{point, rmatrix}], beacons, scanner),
+    do: Enum.map(scanner, &add(rotate(&1, rmatrix), point)) |> MapSet.new() |> MapSet.union(beacons)
+
+  def find_match_point(scanner, beacons) do
+    Enum.flat_map(rotations(), fn rmatrix ->
+      scanner
+      |> Enum.map(fn point -> rotate(point, rmatrix) end)
+      |> Enum.flat_map(&Enum.map(beacons, fn point -> subtract(&1, point) end))
+      |> Enum.frequencies()
+      |> Enum.reject(&(&1 |> elem(1) < 12))
+      |> Enum.map(&{&1 |> elem(0), rmatrix})
+    end)
   end
 
   defp get_input() do
     get_string_input("2021", "19")
+    |> String.split("\n\n", trim: true)
+    |> Enum.map(
+      &(String.split(&1, "\n", trim: true)
+        |> Enum.slice(1..-1)
+        |> Enum.map(fn nums ->
+          String.split(nums, ",", trim: true)
+          |> Enum.map(fn num -> String.to_integer(num) end)
+        end))
+    )
   end
 
-  def solved_status(), do: :unsolved
+  def add([x, y, z], [a, b, c]), do: [x + a, y + b, z + c]
+  def subtract([x, y, z], [a, b, c]), do: [a - x, b - y, c - z]
+  def manhatten_distance([a, b, c], [d, e, f]), do: abs(d - a) + abs(e - b) + abs(f - c)
+
+  defp rotate([x, y, z], [[a, b, c], [d, e, f], [g, h, i]]),
+    do: [x * a + y * b + z * c, x * d + y * e + z * f, x * g + y * h + z * i]
+
+  defp rotations() do
+    [
+      [
+        [-1, 0, 0],
+        [0, 0, -1],
+        [0, -1, 0]
+      ],
+      [
+        [0, -1, 0],
+        [-1, 0, 0],
+        [0, 0, -1]
+      ],
+      [
+        [0, 0, -1],
+        [0, -1, 0],
+        [-1, 0, 0]
+      ],
+      [
+        [-1, 0, 0],
+        [0, -1, 0],
+        [0, 0, 1]
+      ],
+      [
+        [0, -1, 0],
+        [0, 0, 1],
+        [-1, 0, 0]
+      ],
+      [
+        [0, 0, 1],
+        [-1, 0, 0],
+        [0, -1, 0]
+      ],
+      [
+        [-1, 0, 0],
+        [0, 1, 0],
+        [0, 0, -1]
+      ],
+      [
+        [0, 1, 0],
+        [0, 0, -1],
+        [-1, 0, 0]
+      ],
+      [
+        [0, 0, -1],
+        [-1, 0, 0],
+        [0, 1, 0]
+      ],
+      [
+        [-1, 0, 0],
+        [0, 0, 1],
+        [0, 1, 0]
+      ],
+      [
+        [0, 1, 0],
+        [-1, 0, 0],
+        [0, 0, 1]
+      ],
+      [
+        [0, 0, 1],
+        [0, 1, 0],
+        [-1, 0, 0]
+      ],
+      [
+        [1, 0, 0],
+        [0, -1, 0],
+        [0, 0, -1]
+      ],
+      [
+        [0, -1, 0],
+        [0, 0, -1],
+        [1, 0, 0]
+      ],
+      [
+        [0, 0, -1],
+        [1, 0, 0],
+        [0, -1, 0]
+      ],
+      [
+        [1, 0, 0],
+        [0, 0, 1],
+        [0, -1, 0]
+      ],
+      [
+        [0, -1, 0],
+        [1, 0, 0],
+        [0, 0, 1]
+      ],
+      [
+        [0, 0, 1],
+        [0, -1, 0],
+        [1, 0, 0]
+      ],
+      [
+        [1, 0, 0],
+        [0, 0, -1],
+        [0, 1, 0]
+      ],
+      [
+        [0, 1, 0],
+        [1, 0, 0],
+        [0, 0, -1]
+      ],
+      [
+        [0, 0, -1],
+        [0, 1, 0],
+        [1, 0, 0]
+      ],
+      [
+        [1, 0, 0],
+        [0, 1, 0],
+        [0, 0, 1]
+      ],
+      [
+        [0, 1, 0],
+        [0, 0, 1],
+        [1, 0, 0]
+      ],
+      [
+        [0, 0, 1],
+        [1, 0, 0],
+        [0, 1, 0]
+      ]
+    ]
+  end
+
+  def solved_status(), do: :solved
 end
--- test/y2021/day_19_test.exs
+++ test/y2021/day_19_test.exs
@@ -2,19 +2,14 @@ defmodule AocTest.Y2021.Day19Test do
   @moduledoc false
   use ExUnit.Case, async: true
   import Aoc.Y2021.Day19
-  @example []
-  test "Part 1 Example" do
-    assert solve_part1(@example) == :unsolved
-  end
-  test "Part 2 Example" do
-    assert solve_part2(@example) == :unsolved
-  end
-  
+
+  @tag :slow
   test "Part 1 is solved" do
-    assert run_part1() == :unsolved
+    assert run_part1() == 467
   end
 
+  @tag :slow
   test "Part 2 is solved" do
-    assert run_part2() == :unsolved
+    assert run_part2() == 12_226
   end
 end
