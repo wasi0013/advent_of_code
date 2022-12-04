f5f28f5e54f7b9635f4a0fa430f1e6a3d5926120
Wasi Mohammed Abdullah <wasi0013@gmail.com>
2022-12-04 15:19:28 +0600

    marked as solved

--- lib/y2022/day_04.ex
+++ lib/y2022/day_04.ex
@@ -7,28 +7,21 @@ defmodule Aoc.Y2022.Day04 do
   def run_part1(), do: get_input() |> solve_part1()
   def run_part2(), do: get_input() |> solve_part2()
 
-  def solve_part1(data) do
-    data
-    |> Enum.count(&fits?/1)
-  end
-
-  def solve_part2(data) do
-    data
-    |> Enum.count(&overlaps?/1)
-  end
+  def solve_part1(data), do: data |> Enum.count(&fits?/1)
+  def solve_part2(data), do: data |> Enum.count(&overlaps?/1)
 
   def fits?([x1, y1, x2, y2]) when x1 < x2, do: y1 >= y2
   def fits?([x1, _, x1, _]), do: true
   def fits?([_, y1, _, y2]), do: y2 >= y1
 
-  def overlaps?([x1, y1, x2, y2]), do: Enum.empty?(MapSet.intersection(MapSet.new(x1..y1), MapSet.new(x2..y2))) == false
+  def overlaps?([x1, y1, x2, y2]), do:
+    Enum.empty?(MapSet.intersection(MapSet.new(x1..y1), MapSet.new(x2..y2))) == false
 
-  defp get_input() do
+  defp get_input(), do:
     get_string_input("2022", "04")
     |> String.split(["\n", ",", "-"], trim: true)
     |> Enum.map(&String.to_integer/1)
     |> Enum.chunk_every(4, 4, :discard)
-  end
 
-  def solved_status(), do: :unsolved
+  def solved_status(), do: :solved
 end
