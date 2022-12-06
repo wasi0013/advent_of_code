8290caa913e1a65b2e22179d2bc04af317e33ca7
Wasi Mohammed Abdullah <wasi0013@gmail.com>
2022-12-06 21:32:31 +0600

    fixed broken day 06

--- lib/y2022/day_06.ex
+++ lib/y2022/day_06.ex
@@ -5,16 +5,21 @@ defmodule Aoc.Y2022.Day06 do
   import Aoc.Helper.IO
 
   def solve_part1(data), do: data |> detect(4)
-  def solve_part2(data), do: data |> detect(13)
+  def solve_part2(data), do: data |> detect(14)
 
   def detect(data, num),
     do:
       data
       |> Enum.reduce({[], 0}, fn
         _elem, {cache, count} when length(cache) == num -> {cache, count}
-        elem, {cache, count} -> if elem in cache, do: {[elem], count + 1}, else: {[elem | cache], count + 1}
+        elem, {cache, count} -> if elem in cache, do: {clear(cache, elem), count + 1}, else: { [elem | cache], count + 1}
       end)
       |> elem(1)
+
+  def clear(cache, elem), do: clear(cache, elem, [])
+  def clear([elem | _cache], elem, new), do: [elem | new]
+  def clear([other | cache], elem, new), do: clear(cache, elem, new ++ [other])
+
 
   def get_input(), do: get_string_input("2022", "06") |> String.to_charlist()
 
