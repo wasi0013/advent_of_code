e3984fd7dc05a33dd74bab706d4a8746ca83436c
Wasi Mohammed Abdullah <wasi0013@gmail.com>
2022-12-07 21:01:39 +0600

    removed unneccessary func and reformat

--- lib/y2022/day_07.ex
+++ lib/y2022/day_07.ex
@@ -16,37 +16,41 @@ defmodule Aoc.Y2022.Day07 do
       |> parse()
       |> Map.to_list()
       |> Enum.sort(fn {_k1, v1}, {_k2, v2} -> v1 >= v2 end)
-      |> then(fn data -> find_dir(tl(data) |> Enum.reverse(), hd(data), @total_space, @space_required) end)
+      |> then(fn data -> find_smallest(tl(data) |> Enum.reverse(), hd(data), @total_space, @space_required) end)
 
-  def find_dir([{_cur, cur_size} | _data], {_dir, size}, total_space, space_required)
+  def find_smallest([{_cur, cur_size} | _data], {_dir, size}, total_space, space_required)
       when total_space - size + cur_size >= space_required,
       do: cur_size
 
-  def find_dir([_ | data], root, total_space, space_required), do: find_dir(data, root, total_space, space_required)
+  def find_smallest([_ | data], root, total_space, space_required),
+    do: find_smallest(data, root, total_space, space_required)
 
   def parse(data), do: parse(data, %{}, [])
-  def parse([], dirs, _current_dir), do: dirs
-  def parse(["$ cd .." | rest], dirs, [_ | current_dir]), do: parse(rest, dirs, current_dir)
-  def parse(["$ cd " <> dir | rest], dirs, current_dir), do: parse(rest, dirs, [dir | current_dir])
-  def parse(["dir " <> _dir | rest], dirs, current_dir), do: parse(rest, dirs, current_dir)
-  def parse(["$ " <> _dir | rest], dirs, current_dir), do: parse(rest, dirs, current_dir)
+  def parse([], storage_data, _current_path), do: storage_data
+  def parse(["$ cd .." | rest], storage_data, [_ | current_path]), do: parse(rest, storage_data, current_path)
+  def parse(["$ cd " <> dir | rest], storage_data, current_path), do: parse(rest, storage_data, [dir | current_path])
+  def parse(["dir " <> _dir | rest], storage_data, current_path), do: parse(rest, storage_data, current_path)
+  def parse(["$ " <> _dir | rest], storage_data, current_path), do: parse(rest, storage_data, current_path)
 
-  def parse([dir | rest], dirs, current_dir) do
-    updated_dir =
-      Enum.reduce(Enum.reverse(current_dir), {"/", dirs}, fn
-        "/", {n, new} ->
-          {n, Map.put(new, "/", Map.get(new, "/", 0) + get_size(dir))}
-
-        curr, {n, new} ->
-          {n <> "/" <> curr, Map.put(new, n <> "/" <> curr, Map.get(new, n <> "/" <> curr, 0) + get_size(dir))}
-      end)
-      |> elem(1)
-
-    parse(rest, updated_dir, current_dir)
+  def parse([dir | rest], storage_data, current_path) do
+    parse(rest, update_size(current_path, storage_data, dir), current_path)
   end
 
   def get_size(dir), do: dir |> String.split(" ", trim: true) |> hd() |> String.to_integer()
-  def get_dir(current_dir), do: Enum.reverse(current_dir) |> Enum.join("/")
+
+  def update_size(current_path, storage_data, dir),
+    do:
+      current_path
+      |> Enum.reverse()
+      |> Enum.reduce({"/", storage_data}, fn
+        "/", {path, acc_storage} ->
+          {path, Map.put(acc_storage, "/", Map.get(acc_storage, "/", 0) + get_size(dir))}
+
+        curr, {path, acc_storage} ->
+          {path <> "/" <> curr,
+           Map.put(acc_storage, path <> "/" <> curr, Map.get(acc_storage, path <> "/" <> curr, 0) + get_size(dir))}
+      end)
+      |> elem(1)
 
   def get_input() do
     get_string_input("2022", "07")
