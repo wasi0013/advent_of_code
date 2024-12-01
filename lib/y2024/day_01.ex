ae9d4dee6045161ba71db5cd5717fc2e31369837
Wasi Mohammed Abdullah <wasi0013@gmail.com>
2024-12-01 19:26:32 +0600

    advent of code 2024 day 01 solution

---
+++ lib/y2024/day_01.ex
@@ -0,0 +1,28 @@
+defmodule Aoc.Y2024.Day01  do
+  @moduledoc """
+  Solved https://adventofcode.com/2024/day/1
+  """
+  import Aoc.Helper.IO
+  import Aoc.Helper.Util
+
+  def solve_part1(data) do
+    data
+    |> rotate90()
+    |> then (fn [list1, list2] -> [Enum.sort(list1), Enum.sort(list2)] |>  Enum.zip() |> Enum.map(&Tuple.to_list/1) |> Enum.map(fn [a, b] -> abs(a - b)  end) |> Enum.sum() end)
+
+  end
+
+  def solve_part2(data) do
+    data
+    |> rotate90()
+    |> then (fn [list1, list2] -> Enum.map(list1, fn a -> a * Enum.count(list2, &(a==&1)) end) |> Enum.sum() end)
+  end
+
+  def get_input() do
+    get_string_input("2024", "01")
+    |> String.split("\n")
+    |> Enum.map(fn string -> string |> String.split("   ") |> Enum.map(&String.to_integer/1) end)
+  end
+
+  def solved_status(), do: :unsolved
+end
---
+++ test/y2024/day_01_test.exs
@@ -0,0 +1,13 @@
+defmodule AocTest.Y2024.Day01Test do
+  @moduledoc false
+  use ExUnit.Case, async: true
+  import Aoc.Y2024.Day01
+
+  test "Part 1 is solved" do
+    assert solve_part1(get_input()) == 1388114
+  end
+
+  test "Part 2 is solved" do
+    assert solve_part2(get_input()) == 235_298_53
+  end
+end
